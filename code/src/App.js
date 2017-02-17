f2fcf55fe6a8297f8eb3737fdcc5625cf54d4b05
Nicole Rauch <nicole.m@gmx.de>
2017-02-17 22:09:44 +0100

    Simplified index.html and App, so that the participants can start implementing the first simple component straight away.

--- code/src/App.js
+++ code/src/App.js
@@ -1,25 +1,5 @@
 import React, {Component} from "react";
-import { connect } from "react-redux";
 
-import HelloWorld from "./HelloWorld";
-import UserList from "./UserList";
-import UserCreation from "./UserCreation";
-import * as Actions from "./Actions";
+export default class App extends Component {
 
-export class AppComponent extends Component {
-
-  render() {
-    const {users, dispatch} = this.props;
-
-    return (
-      <div>
-        <HelloWorld />
-        <UserList users={users} />
-        <UserCreation submitUser={user => dispatch(Actions.addUser(user))}
-        />
-      </div>
-    );
-  }
 }
-
-export default connect(state => state)(AppComponent);
--- code/src/index.js
+++ code/src/index.js
@@ -1,10 +1,22 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
+
+import App from "./App";
+
+ReactDOM.render(
+    <App />
+    , document.getElementById("start")
+);
+
+
+
+/*
 import {Provider} from "react-redux";
 import {createStore, applyMiddleware} from "redux";
 import createLogger from "redux-logger";
 
 import App from "./App";
+
 import reducers from './reducers'
 
 const store = createStore(reducers,
@@ -13,11 +25,12 @@ const store = createStore(reducers,
   )
 );
 
+ ReactDOM.render(
+ <Provider store={store}>
+ <App />
+ </Provider>
+ , document.getElementById("start")
+ );
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-  , document.getElementById("start")
-);
+ */
 
